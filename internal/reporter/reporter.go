7f7f03291909ddeef40e9bedfacfcbb313ec9e4b
B1ZON-c0de <roman.andreicko@icloud.com>
2026-03-20 22:42:41 +0700

    add reporter pkg

--- internal/reporter/reporter.go
+++ internal/reporter/reporter.go
@@ -1,1 +1,45 @@
 package reporter
+
+import (
+	"fmt"
+	"io"
+	"strings"
+)
+
+const (
+	SuccessEnvMsg   = "ОК: Все переменные синхронизированы"
+	UnsuccessEnvMsg = "Расхождения найдены:\n\nОтсутствуют в .env.example (добавьте их):\n"
+)
+
+func PrintCheckedEnv(w io.Writer, analyzedEnv map[string]bool) {
+	var unfoundVariables []string
+
+	//Проверяем есть ли ключи со значением false
+	for k := range analyzedEnv {
+		if !analyzedEnv[k] {
+			unfoundVariables = append(unfoundVariables, k)
+		}
+	}
+
+	//Если всё ок то выводим сообщение об успешной проверке
+	if len(unfoundVariables) == 0 {
+		fmt.Fprint(w, SuccessEnvMsg)
+		return
+	}
+
+	//Если были найдены расхождения то выводим их
+	unfoundVarString := formatUnfoundVar(unfoundVariables)
+
+	fmt.Fprint(w, unfoundVarString)
+}
+
+func formatUnfoundVar(unfoundVariables []string) string {
+	var sb strings.Builder
+	sb.WriteString(UnsuccessEnvMsg)
+
+	for _, k := range unfoundVariables {
+		sb.WriteString(fmt.Sprintf("- %s\n", k))
+	}
+
+	return sb.String()
+}
--- internal/reporter/reporter_test.go
+++ internal/reporter/reporter_test.go
@@ -1,1 +1,47 @@
 package reporter_test
+
+import (
+	"bytes"
+	"envsheriff/internal/reporter"
+	"testing"
+)
+
+func TestPrintCheckedEnv(t *testing.T) {
+	t.Run("Все переменные одинаковы", func(t *testing.T) {
+		var buf bytes.Buffer
+
+		mockAnalyzedEnv := map[string]bool{
+			"DEBUG": true,
+			"INFO":  true,
+		}
+
+		reporter.PrintCheckedEnv(&buf, mockAnalyzedEnv)
+
+		want := reporter.SuccessEnvMsg
+
+		assertBuf(t, &buf, want)
+	})
+
+	t.Run("Одна перемнная имеет статус false", func(t *testing.T) {
+		var buf bytes.Buffer
+
+		mockAnalyzedEnv := map[string]bool{
+			"DEBUG": false,
+			"INFO":  true,
+		}
+
+		reporter.PrintCheckedEnv(&buf, mockAnalyzedEnv)
+
+		want := "Расхождения найдены:\n\nОтсутствуют в .env.example (добавьте их):\n- DEBUG\n"
+
+		assertBuf(t, &buf, want)
+	})
+}
+
+func assertBuf(t *testing.T, buf *bytes.Buffer, want string) {
+	t.Helper()
+
+	if want != buf.String() {
+		t.Errorf("Ожидали %q Получили %q", want, buf.String())
+	}
+}
